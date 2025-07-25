9449abe09be570060b4b19f7c08b13bc2fb3f792
Walter-Vigiambiental <walteralves.vigiambientalmoc@gmail.com>
2025-07-25 11:29:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -152,5 +152,6 @@ if not locais_alerta.empty:
     for _, row in locais_alerta.iterrows():
         st.warning(
             st.warning(
+    st.warning(
     f"⚠️ **{row['LOCAL']}** está há **{int(row['DIAS_SEM_ENTREGA'])} dias** sem entrega (última em {row['DATA'].strftime('%d/%m/%Y')})"
 )
